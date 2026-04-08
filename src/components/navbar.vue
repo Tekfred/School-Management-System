c8530204ec1a95e00cda2fb74b8f47260711e8dd
Tekfred <asagodfred101@gmail.com>
2026-04-08 21:18:04 +0000

    updated package downloader

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -35,7 +35,7 @@ const closeDropdown = () => {
     <div class="bg-white shadow-sm border-b">
      
     </div>
-        <div class="px-8 py-6 flex justify-between items-center">
+        <div class="px-8 py-4 flex justify-between items-center">
             <h1 class="text-2xl font-semibold text-gray-800 ">School Overview
                 <input type="search" class="w-100 bg-[#f6f6f8] border-none rounded-xl py-2 md:py-3 px-4 md:px-8 lg:px-12 focus:ring-2 focus:ring-purple-500 transition-all text-xs md:text-sm" placeholder="Search Students, Teachers....">
             </h1>
