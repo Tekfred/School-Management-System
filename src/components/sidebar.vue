1df029640ae59e2ce4b92ec450088371b8024e87
Tekfred <asagodfred101@gmail.com>
2026-07-12 15:22:06 +0000

    feat:/ sidebar :removed settings from object array list

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -30,12 +30,6 @@ const menuSidebar = [
     path: '/app/finance',
     icon: 'account_balance_wallet',
   },
-  {
-    id: 5,
-    name: 'Settings',
-    path: '/app/settings',
-    icon: 'settings',
-  },
 ];
 </script>
 
